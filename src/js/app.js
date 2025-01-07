2ec16e19145b1e89425077ed467afb43afad9210
devluizmarcolino <devluizmarcolino@gmail.com>
2025-01-07 02:32:44 -0300

    feat: seção contatos atualizada

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const missionIcon = document.getElementById("mission-icon");
-  const visionIcon = document.getElementById("vision-icon");
-  const valuesIcon = document.getElementById("values-icon");
-
-  const icons = [missionIcon, visionIcon, valuesIcon];
-
-  icons.forEach((icon) => {
-    icon.addEventListener("mouseenter", () => {
-      icon.classList.add("animate");
-    });
-
-    icon.addEventListener("mouseleave", () => {
-      icon.classList.remove("animate");
-    });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const serviceItems = document.querySelectorAll(".service-item");
+  const serviceItems = document.querySelectorAll(".service");
   const benefitCards = document.querySelectorAll(".benefits-card");
-  const aboutIcons = document.querySelectorAll(".about-icon");
+  const contactItems = document.querySelectorAll(".contact-item");
+  const contactForm = document.querySelector(".contact-form");
 
   ScrollReveal().reveal(".home-content h1", {
     duration: 1000,
@@ -55,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
     reset: true,
   });
 
-  ScrollReveal().reveal(".about-card-icon", {
+  ScrollReveal().reveal(".about-icon", {
     duration: 1000,
     origin: "bottom",
     distance: "50px",
@@ -72,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
     reset: true,
   });
 
-  ScrollReveal().reveal(".service-item", {
+  ScrollReveal().reveal(".service", {
     duration: 1000,
     origin: "left",
     distance: "50px",
@@ -97,4 +80,73 @@ document.addEventListener("DOMContentLoaded", () => {
     interval: 200,
     reset: true,
   });
+
+  ScrollReveal().reveal(".contact-description", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".contact-item", {
+    duration: 1000,
+    origin: "left",
+    distance: "50px",
+    easing: "ease-in-out",
+    interval: 200,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".contact-form", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    delay: 500,
+    reset: true,
+  });
+
+  // Form validation and data capture
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      alert("Por favor, insira um endereço de email válido.");
+      return;
+    }
+
+    // Enviar email usando EmailJS
+    emailjs
+      .send("service_wlfv16d", "template_qkmtx1s", {
+        from_name: name,
+        from_email: email,
+        message: message,
+      })
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("Mensagem enviada com sucesso!");
+          form.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          alert("Falha ao enviar a mensagem. Por favor, tente novamente.");
+        }
+      );
+  });
+
+  function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
 });
