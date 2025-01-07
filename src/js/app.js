a94a5511a07ce11bf00da806622a434b8d7ddade
devluizmarcolino <devluizmarcolino@gmail.com>
2025-01-07 01:54:40 -0300

    feat: seção serviços concluído

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,86 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const serviceItems = document.querySelectorAll(".service-item");
+  const benefitCards = document.querySelectorAll(".benefits-card");
+  const aboutIcons = document.querySelectorAll(".about-icon");
+
+  ScrollReveal().reveal(".home-content h1", {
+    duration: 1000,
+    origin: "left",
+    distance: "50px",
+    easing: "ease-in-out",
+    reset: true,
+  });
+
+  ScrollReveal().reveal("#learn-more-btn", {
+    duration: 1000,
+    origin: "right",
+    distance: "50px",
+    easing: "ease-in-out",
+    delay: 500,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".about-content h2", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".about-description", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    delay: 500,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".about-card-icon", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    interval: 200,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".services-description", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".service-item", {
+    duration: 1000,
+    origin: "left",
+    distance: "50px",
+    easing: "ease-in-out",
+    interval: 200,
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".benefits-subsection", {
+    duration: 1000,
+    origin: "bottom",
+    distance: "50px",
+    easing: "ease-in-out",
+    reset: true,
+  });
+
+  ScrollReveal().reveal(".benefits-card", {
+    duration: 1000,
+    origin: "right",
+    distance: "50px",
+    easing: "ease-in-out",
+    interval: 200,
+    reset: true,
+  });
+});
